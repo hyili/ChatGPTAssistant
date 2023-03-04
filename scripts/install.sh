433b62a1f1d4fa33a9399eed007fe30ba1ccca89
hyili <a19931031@gmail.com>
2023-03-04 22:15:53 +0800

    bugfix. fix bug in install.sh script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip3 insntall -r requirements.txt
-apt install sox libsox-fmt-all
+pip3 install -r requirements.txt
+sudo apt install sox libsox-fmt-all
 mkdir -p record private audio
